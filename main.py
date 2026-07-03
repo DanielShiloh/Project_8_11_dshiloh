2f49589cd73a6a1c2caca450c603f3b7ba6c7b73
Daniel Shiloh <dshiloh@cscc.edu>
2026-07-03 11:24:36 -0400

    Created basic human/dog classes

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ Daniel Shiloh
 July 3, 2026
 """
 
+class Human:
+    """represents dog owner"""
+
+    def __init__(self, name: str, phone: str):
+        """create owner with given info"""
+        self.name = name
+        self.phone = phone
+
+class Dog:
+    """single patient at clinic"""
+
+    def __init__(self, name: str, human: Human):
+        """create dog with given info and Human object"""
+        self.name = name
+        self.human = human
