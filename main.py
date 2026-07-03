ab8c0268d73844aa3982068f4ebfddd5e6af8f59
Daniel Shiloh <dshiloh@cscc.edu>
2026-07-03 17:20:14 -0400

    Added validation classes for user inputs

--- main.py
+++ main.py
@@ -6,29 +6,21 @@ July 3, 2026
 """
 
 #TO DO list:
-# registration: mostly just actually read/write to file w json
 # main: validate inputs, add try/except, refactor
 # test: all test cases
 
 import string
 from registration import Registration
-
-def get_clean_name(name: str):
-    return name.strip().upper()
-    
-def get_clean_phone(phone: str):
-    clean_phone = ""
-    for char in phone:
-        if char in string.digits:
-            clean_phone += char
-    return clean_phone
+from datetime import datetime
 
 def print_header():
+    """prints at top of output"""
     print("-" * 40)
     print("    Daniel's Dog Clinic: Registration    ")
     print("-" * 40)
 
 def print_menu():
+    """prints options for initial user input"""
     print("\nMenu:")
     print("1. Register new human")
     print("2. Register new dog (requires Human ID)")
@@ -36,13 +28,49 @@ def print_menu():
     print("4. Search registered dogs")
     print("5. Exit")
 
+def get_clean_name(name: str):
+    """format user input consistently; use before storing value"""
+    return name.strip().upper()
+    
+def get_clean_phone(phone: str):
+    """format user input consistently; use before storing value"""
+    clean_phone = ""
+    for char in phone:
+        if char in string.digits:
+            clean_phone += char
+    return clean_phone
+
+def get_non_empty_input(prompt):
+    """repeat prompt until user's response isn't blank"""
+    while True:
+        user_input = input(prompt)
+        if user_input != "":
+            return user_input
+        print("\nThis field cannot be left blank.  Please try again.")
+
+def validate_sex(sex_str):
+    """check if user-inputted sex matches reasonable options"""
+    if sex_str.upper() in ['M', 'F', 'MALE', 'FEMALE', 'BOY', 'GIRL']:
+        return True
+    return False
+
+def validate_dob(date_str):
+    """check if user-inputted date of birth is a valid yyyy-mm-dd date"""
+    try:
+        datetime.strptime(date_str, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
 def main():
+    """manage user interaction, adding to and searching in registry"""
 
     clinic = Registration()
 
     print_header()
 
     while True:
+        
         print_menu()
         choice = input("Select an option (1-5): ")
 
@@ -58,11 +86,16 @@ def main():
     
         elif choice == '2':
             print("\n--New Dog Registration--")
-            human_id = get_clean_name(input("Enter owner id (eg H-1): "))
+
+            human_id = get_clean_name(input("Enter human id (eg H-1): "))
+            if human_id in clinic.humans:
+                print("Human ID not found.  Please register the human.")
+
+
             #TO DO: validate these inputs
             name = get_clean_name(input("Dog's name: "))
             breed = input("Breed: ")
-            sex = input("Sex: ")
+            sex = input("Sex (m/f): ")
             dob = input("Estimated date of birth (YYYY-MM-DD): ")
             potential_id = clinic.register_dog(name, breed, sex, dob, human_id)
             print(f"\n{name.title()}'s ID is {potential_id}.")
